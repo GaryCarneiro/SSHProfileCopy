7b8450d7f4ded31bc8c921a9b565128b4e77ae18
Garfield Carneiro <gcarneiro@rocketfuel.com>
2016-09-26 07:03:09 -0400

    Implementing Debugger. Doesnt work :(

--- bin/rssh.rb
+++ bin/rssh.rb
@@ -6,28 +6,41 @@ require 'net/scp'
 require 'highline/import'
 require 'trollop'
 
-
+=begin
+def dp(message)
+  if parser.debug
+    puts message
+  end
+end
+=end
 
 
 class Parser
 
+  attr_reader :debug
   def initialize()
     @hostname = ''
     @username = ''
     @configfile = ''
+    @debug = false
   end
     
   def parse()
-    opts = Trollop::options do
+    @opts = Trollop::options do
       opt :debug, "Debug Mode"
       opt :conffile, "Specify Config file if not using default", :type => :string
       opt :username, "Specify Username to use for SSH Connections. Defaults to logged in username", :type => :string
       opt :hostname, "Specify Hostname to use for SSH Connections", :type => :string
     end
 
+    if @opts[:debug] 
+      @debug = true
+    end
+
+
     # Validating existence of username
     # If not present getting it from system
-    if not opts[:username].nil?
+    if not @opts[:username].nil?
       @username = opts[:username]
     else
       @username = Etc.getlogin()
@@ -37,11 +50,11 @@ class Parser
     # If --hostname is given it overrides 2nd arg
     # if neither argument or option was passed, we better exit
     
-    if not opts[:hostname].nil?
+    if not @opts[:hostname].nil?
       @hostname = opts[:hostname]
-    elsif opts[:hostname].nil? and not ARGV[0].nil?
+    elsif @opts[:hostname].nil? and not ARGV[0].nil?
       @hostname = ARGV[0]
-    elsif opts[:hostname].nil? and ARGV[0].nil?
+    elsif @opts[:hostname].nil? and ARGV[0].nil?
       puts "Hostname not specified"
       exit 2
     end
@@ -49,7 +62,7 @@ class Parser
     # Config file presence
 
     baseDir = File.dirname(__FILE__)  + '/..'
-    if not opts[:conffile].nil?
+    if not @opts[:conffile].nil?
       @conffile = opts[:conffile]
     else
       @conffile = "#{baseDir}/conf/source.files"
@@ -60,6 +73,20 @@ class Parser
   end # parse() method ends
 
 end
+
+class Debug
+
+  def initialize(debugFlag, message)
+    @debug = debugFlag
+    @msg = message
+  end
+  
+    if @debug
+      puts @msg
+    end
+
+end
+
 
 
 class SSHProfileCopy
@@ -74,8 +101,8 @@ class SSHProfileCopy
 
 
   def getFileList(confFile)
-    puts "Yeah the conf file is #{@confFile}"
-    puts File.read(@confFile)
+    # puts "Yeah the conf file is #{@confFile}"
+    # puts File.read(@confFile)
   end
 
   def getSSHCredentials
@@ -98,7 +125,8 @@ end
 
 parser  = Parser.new
 host, user, conf = parser.parse()
-puts host, user, conf
+
+Debug.new(parser.debug, "Hello")
 
 sshCopier = SSHProfileCopy.new(host, user, conf)
 # p sshCopier
